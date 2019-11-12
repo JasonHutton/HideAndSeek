dd0ff8f04a599b5db3e607b39f507c0ad466ac31
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-11-12 13:08:29 -0800

    Tag walls. Move tanks up a bit. Get movementeffector working.

--- Assets/Scripts/MovementEffector.cs
+++ Assets/Scripts/MovementEffector.cs
@@ -12,17 +12,56 @@ public class MovementEffector : MonoBehaviour
     public bool MoveUp;
     public bool MoveDown;
 
+    private Rigidbody rb;
+
     void Start()
     {
         ControllerScript = GetComponent<InputControllerI>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveLeft = ControllerScript.MoveLeft;
-        MoveRight = ControllerScript.MoveRight;
-        MoveUp = ControllerScript.MoveUp;
-        MoveDown = ControllerScript.MoveDown;
+        if (ControllerScript != null)
+        {
+            MoveLeft = ControllerScript.MoveLeft;
+            MoveRight = ControllerScript.MoveRight;
+            MoveUp = ControllerScript.MoveUp;
+            MoveDown = ControllerScript.MoveDown;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 position = transform.position;
+
+        if (MoveLeft)
+            position.x -= 0.1f;
+
+        if (MoveRight)
+            position.x += 0.1f;
+
+        if (MoveUp)
+            position.z += 0.1f;
+
+        if (MoveDown)
+            position.z -= 0.1f;
+
+        transform.position = position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Barrier")
+        {
+            rb.velocity = Vector3.zero;
+            Debug.Log("Collided with a Barrier!");
+        }
     }
 }
