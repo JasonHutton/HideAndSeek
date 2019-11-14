7296dc74489ba1a0aeddd75026d7bf635e109b2a
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-11-14 11:15:52 -0800

    Just moving partially done stuff to a remote branch to share.

--- Assets/Scripts/AIInputController.cs
+++ Assets/Scripts/AIInputController.cs
@@ -17,9 +17,20 @@ public class AIInputController : InputControllerI
     // Update is called once per frame
     void Update()
     {
+        //RandomWanderViaButtons();
+        Wander();
+    }
+
+    void Wander()
+    {
+
+    }
+
+    void RandomWanderViaButtons()
+    {
         timer += Time.deltaTime;
 
-        if(timer > directionChangeInterval)
+        if (timer > directionChangeInterval)
         {
             timer = 0;
             directionChangeInterval = Random.Range(minDirectionChangeInterval, maxDirectionChangeInterval);
@@ -58,6 +69,92 @@ public class AIInputController : InputControllerI
                     break;
             }
         }
-
     }
+
+    /*
+    // https://github.com/libgdx/gdx-ai/wiki/Steering-Behaviors
+    public static float calculateOrientationFromLinearVelocity(Steerable<T> character)
+    {
+        // If we haven't got any velocity, then we can do nothing.
+        if (character.getLinearVelocity().isZero(character.getZeroLinearSpeedThreshold()))
+            return character.getOrientation();
+
+        return character.vectorToAngle(character.getLinearVelocity());
+    }
+
+
+    private void applySteering(SteeringAcceleration<Vector2> steering, float time)
+    {
+        // Update position and linear velocity. Velocity is trimmed to maximum speed
+        this.position.mulAdd(linearVelocity, time);
+        this.linearVelocity.mulAdd(steering.linear, time).limit(this.getMaxLinearSpeed());
+
+        // Update orientation and angular velocity
+        if (independentFacing)
+        {
+            this.orientation += angularVelocity * time;
+            this.angularVelocity += steering.angular * time;
+        }
+        else
+        {
+            // For non-independent facing we have to align orientation to linear velocity
+            float newOrientation = calculateOrientationFromLinearVelocity(this);
+            if (newOrientation != this.orientation)
+            {
+                this.angularVelocity = (newOrientation - this.orientation) * time;
+                this.orientation = newOrientation;
+            }
+        }
+    }*/
+}
+
+class KinematicWander
+{   struct Static
+    {
+        Vector3 position;
+        float orientation;
+    }
+
+    struct Kinematic
+    {
+        Vector3 position;
+        float orientation;
+        Vector3 velocity;
+        float rotation;
+    }
+
+    struct SteeringOutput
+    {
+        Vector3 linear;
+        float angular;
+    }
+
+    // Holds the static data for the character
+    private Static character;
+
+    // Holds the maximum speed the character can travel
+    public float maxSpeed;
+
+    // Holds the maximum rotation speed we’d like, probably
+    // should be smaller than the maximum possible, to allow
+    // a leisurely change in direction
+    public float maxRotation;
+
+    KinematicSteeringOutput getSteering()
+    {
+
+        // Create the structure for output
+        steering = new KinematicSteeringOutput()
+
+    // Get velocity from the vector form of the orientation
+        steering.velocity = maxSpeed *
+    character.orientation.asVector()
+    Steering Behaviors 57
+
+    // Change our orientation randomly
+        steering.rotation = randomBinomial() * maxRotation
+
+    // Output the steering
+        return steering
+            }
 }
